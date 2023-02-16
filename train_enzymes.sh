0965af7d13150919d42b0a60d0e5aa969b3b9f67
Genjia Liu <63838298+gjliu9@users.noreply.github.com>
2023-02-16 14:57:30 +0800

    Update train_enzymes.sh

--- train_enzymes.sh
+++ train_enzymes.sh
@@ -1,10 +1,10 @@
 
 k=5
-python pGST_classification_ENZYMES.py --variation $k --sample umin --gstcoe_norml MS
-python pGST_classification_ENZYMES.py --variation $k --sample umin --gstcoe_norml none
-python pGST_classification_ENZYMES.py --variation $k --sample umin --gstcoe_norml MS --feature_choosen 3
-python pGST_classification_ENZYMES.py --variation $k --sample umin --gstcoe_norml none --feature_choosen 3
-python pGST_classification_ENZYMES.py --variation $k --sample umax --gstcoe_norml MS
-python pGST_classification_ENZYMES.py --variation $k --sample umax --gstcoe_norml none
-python pGST_classification_ENZYMES.py --variation $k --sample umax --gstcoe_norml MS --feature_choosen 3
-python pGST_classification_ENZYMES.py --variation $k --sample umax --gstcoe_norml none --feature_choosen 3
+python mGST_classification_ENZYMES.py --variation $k --sample umin --gstcoe_norml MS
+python mGST_classification_ENZYMES.py --variation $k --sample umin --gstcoe_norml none
+python mGST_classification_ENZYMES.py --variation $k --sample umin --gstcoe_norml MS --feature_choosen 3
+python mGST_classification_ENZYMES.py --variation $k --sample umin --gstcoe_norml none --feature_choosen 3
+python mGST_classification_ENZYMES.py --variation $k --sample umax --gstcoe_norml MS
+python mGST_classification_ENZYMES.py --variation $k --sample umax --gstcoe_norml none
+python mGST_classification_ENZYMES.py --variation $k --sample umax --gstcoe_norml MS --feature_choosen 3
+python mGST_classification_ENZYMES.py --variation $k --sample umax --gstcoe_norml none --feature_choosen 3
