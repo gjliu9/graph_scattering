075434b7d45d3b7ae629c50aa56326197e6ba77c
Genjia Liu <63838298+gjliu9@users.noreply.github.com>
2023-02-16 14:57:52 +0800

    Update train_proteins.sh

--- train_proteins.sh
+++ train_proteins.sh
@@ -1,6 +1,6 @@
 k=5
 
-# python pGST_classification_proteins.py --variation $k --sample umin --gstcoe_norml MS
-python pGST_classification_proteins.py --variation $k --sample umin --gstcoe_norml none
-python pGST_classification_proteins.py --variation $k --sample umax --gstcoe_norml MS
-python pGST_classification_proteins.py --variation $k --sample umax --gstcoe_norml none
+# python mGST_classification_proteins.py --variation $k --sample umin --gstcoe_norml MS
+python mGST_classification_proteins.py --variation $k --sample umin --gstcoe_norml none
+python mGST_classification_proteins.py --variation $k --sample umax --gstcoe_norml MS
+python mGST_classification_proteins.py --variation $k --sample umax --gstcoe_norml none
